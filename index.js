00e38667c837f626b738ad80f46e20bfa7a421e5
Ishan Loya <ishanloya@gmail.com>
2018-10-02 16:51:23 +0530

    Get http method and trim '/' using a regex

--- index.js
+++ index.js
@@ -15,12 +15,19 @@ const server = http.createServer((req, res) => {
     var parsedUrl = url.parse(req.url, true);
 
     // get the path from the url
+    var path = parsedUrl.pathname;
 
+    // trims any trailing and leading '/'s
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    
+    // get http method
+    var method = req.method.toLowerCase();
 
     // send the response
     res.end('Hello World!\n');
 
     // log the request path
+    console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
 });
 
 // start the server and listen on port 3000
