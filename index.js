b69070749868084856ab6a0c5b68e1dd676c2c10
Ishan Loya <ishanloya@gmail.com>
2018-10-03 09:51:28 +0530

    Setup handlers, get query string object, headers, etc.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // the server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -20,17 +21,67 @@ const server = http.createServer((req, res) => {
     // trims any trailing and leading '/'s
     var trimmedPath = path.replace(/^\/+|\/+$/g,'');
     
+    // get query string as an object
+    var queryStringObject = parsedUrl.query;
+    
     // get http method
     var method = req.method.toLowerCase();
 
-    // send the response
-    res.end('Hello World!\n');
+    // get headers as an object
+    var headers = req.headers;
 
-    // log the request path
-    console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
+    // get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    // binding on data event of stream
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    // binding on end event of stream
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // choose handler request should go to, if not found go to not found handler
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // construct data object to send to handler
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        // send the response
+        res.end('Hello World!\n');
+        // log the request path
+        console.log(buffer);
+    });
 });
 
 // start the server and listen on port 3000
 server.listen(3000, () => {
     console.log('The server is listening on port 3000 now');
 });
+
+// define the handlers
+var handlers = {
+
+};
+
+// sample handler
+handlers.sample = (data, callback) => {
+    // callback a http status code, and a payload object
+    callback(406, {'name': 'sample handler'});
+};
+
+// not found handler
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+// define a rquest router
+var router = {
+    'sample': handlers.sample
+};
