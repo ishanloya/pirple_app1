8165ef0c20b556a652b2d4cec1cf2f1bb1686d63
Ishan Loya <ishanloya@gmail.com>
2018-10-04 13:29:25 +0530

    Add tokens

--- index.js
+++ index.js
@@ -109,5 +109,6 @@ var unifiedServer = (req, res) => {
 // define a rquest router
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 };
