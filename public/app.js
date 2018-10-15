3bb17433669dc20e0c265e512675120f80c9215e
Ishan Loya <ishanloya@gmail.com>
2018-10-15 10:35:02 +0530

    Add public folder, start AJAX client

---
+++ public/app.js
@@ -0,0 +1,15 @@
+/*
+ * Front-end js for the app
+ *
+ */
+
+//  container for the front-end application
+ var app = {};
+
+//  config
+app.config = {
+    'sessionToken': false
+};
+
+// AJAX client (for the restful API)
+app.client = {};
